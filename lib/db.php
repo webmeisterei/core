fb851beb2ec43819a2fa4b32b49406df41104b87
Bart Visscher <bartv@thisnet.nl>
2013-02-25 23:45:07 +0100

    Cleanup: remove code for multiple database backends

--- lib/db.php
+++ lib/db.php
@@ -40,63 +40,24 @@ class DatabaseException extends Exception {
  * Doctrine with some adaptions.
  */
 class OC_DB {
-	const BACKEND_DOCTRINE=2;
-
 	/**
 	 * @var \Doctrine\DBAL\Connection
 	 */
 	static private $connection; //the prefered connection to use, only Doctrine
-	static private $backend=null;
-	/**
-	 * @var Doctrine
-	 */
-	static private $DOCTRINE=null;
 
 	static private $type=null;
 
 	/**
-	 * check which backend we should use
-	 * @return int BACKEND_DOCTRINE
-	 */
-	private static function getDBBackend() {
-		return self::BACKEND_DOCTRINE;
-	}
-
-	/**
 	 * @brief connects to the database
-	 * @param int $backend
 	 * @return bool true if connection can be established or false on error
 	 *
 	 * Connects to the database as specified in config.php
 	 */
-	public static function connect($backend=null) {
+	public static function connect() {
 		if(self::$connection) {
 			return true;
 		}
-		if(is_null($backend)) {
-			$backend=self::getDBBackend();
-		}
-		if($backend==self::BACKEND_DOCTRINE) {
-			$success = self::connectDoctrine();
-			self::$connection=self::$DOCTRINE;
-			self::$backend=self::BACKEND_DOCTRINE;
-		}
-		return $success;
-	}
 
-	/**
-	 * connect to the database using doctrine
-	 *
-	 * @return bool
-	 */
-	public static function connectDoctrine() {
-		if(self::$connection) {
-			if(self::$backend!=self::BACKEND_DOCTRINE) {
-				self::disconnect();
-			} else {
-				return true;
-			}
-		}
 		// The global data we need
 		$name = OC_Config::getValue( "dbname", "owncloud" );
 		$host = OC_Config::getValue( "dbhost", "" );
@@ -110,7 +71,7 @@ class OC_DB {
 		}
 
 		// do nothing if the connection already has been established
-		if(!self::$DOCTRINE) {
+		if(!self::$connection) {
 			$config = new \Doctrine\DBAL\Configuration();
 			switch($type) {
 				case 'sqlite':
@@ -177,7 +138,7 @@ class OC_DB {
 			$connectionParams['wrapperClass'] = 'OC\DB\Connection';
 			$connectionParams['table_prefix'] = OC_Config::getValue( "dbtableprefix", "oc_" );
 			try {
-				self::$DOCTRINE = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+				self::$connection = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 				if ($type === 'sqlite' || $type === 'sqlite3') {
 					// Sqlite doesn't handle query caching and schema changes
 					// TODO: find a better way to handle this
@@ -210,14 +171,12 @@ class OC_DB {
 	static public function prepare( $query, $limit=null, $offset=null ) {
 		self::connect();
 		// return the result
-		if (self::$backend == self::BACKEND_DOCTRINE) {
-			try {
-				$result=self::$connection->prepare($query, $limit, $offset);
-			} catch(\Doctrine\DBAL\DBALException $e) {
-				throw new \DatabaseException($e->getMessage(), $query);
-			}
-			$result=new DoctrineStatementWrapper($result);
+		try {
+			$result=self::$connection->prepare($query, $limit, $offset);
+		} catch(\Doctrine\DBAL\DBALException $e) {
+			throw new \DatabaseException($e->getMessage(), $query);
 		}
+		$result=new DoctrineStatementWrapper($result);
 		return $result;
 	}
 
@@ -238,7 +197,6 @@ class OC_DB {
 
 	/**
 	 * @brief Disconnect
-	 * @return bool
 	 *
 	 * This is good bye, good bye, yeah!
 	 */
@@ -247,8 +205,6 @@ class OC_DB {
 		if(self::$connection) {
 			self::$connection->close();
 		}
-
-		return true;
 	}
 
 	/** else {
@@ -392,9 +348,9 @@ class OC_DB {
 	 * @return string
 	 */
 	public static function getErrorMessage($error) {
-		if (self::$backend==self::BACKEND_DOCTRINE and self::$DOCTRINE) {
-			$msg = self::$DOCTRINE->errorCode() . ': ';
-			$errorInfo = self::$DOCTRINE->errorInfo();
+		if (self::$connection) {
+			$msg = self::$connection->errorCode() . ': ';
+			$errorInfo = self::$connection->errorInfo();
 			if (is_array($errorInfo)) {
 				$msg .= 'SQLSTATE = '.$errorInfo[0] . ', ';
 				$msg .= 'Driver Code = '.$errorInfo[1] . ', ';
