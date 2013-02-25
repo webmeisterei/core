e49abd59133aa591ed2501a6d892718129466829
Bart Visscher <bartv@thisnet.nl>
2013-02-25 21:35:11 +0100

    Move prepared query cache handling to Connection wrapper

--- lib/db.php
+++ lib/db.php
@@ -42,9 +42,6 @@ class DatabaseException extends Exception {
 class OC_DB {
 	const BACKEND_DOCTRINE=2;
 
-	static private $preparedQueries = array();
-	static private $cachingEnabled = true;
-
 	/**
 	 * @var \Doctrine\DBAL\Connection
 	 */
@@ -101,7 +98,6 @@ class OC_DB {
 				return true;
 			}
 		}
-		self::$preparedQueries = array();
 		// The global data we need
 		$name = OC_Config::getValue( "dbname", "owncloud" );
 		$host = OC_Config::getValue( "dbhost", "" );
@@ -183,6 +179,11 @@ class OC_DB {
 			$connectionParams['table_prefix'] = OC_Config::getValue( "dbtableprefix", "oc_" );
 			try {
 				self::$DOCTRINE = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+				if ($type === 'sqlite' || $type === 'sqlite3') {
+					// Sqlite doesn't handle query caching and schema changes
+					// TODO: find a better way to handle this
+					self::$connection->disableQueryStatementCaching();
+				}
 			} catch(\Doctrine\DBAL\DBALException $e) {
 				OC_Log::write('core', $e->getMessage(), OC_Log::FATAL);
 				OC_User::setUserId(null);
@@ -226,12 +227,7 @@ class OC_DB {
 			} else {
 				$query.=$limitsql;
 			}
-		} else {
-			if (isset(self::$preparedQueries[$query]) and self::$cachingEnabled) {
-				return self::$preparedQueries[$query];
-			}
 		}
-		$rawQuery = $query;
 
 		// Optimize the query
 		$query = self::processQuery( $query );
@@ -245,12 +241,6 @@ class OC_DB {
 				throw new \DatabaseException($e->getMessage(), $query);
 			}
 			$result=new DoctrineStatementWrapper($result);
-		}
-		if ((is_null($limit) || $limit == -1) and self::$cachingEnabled ) {
-			$type = OC_Config::getValue( "dbtype", "sqlite" );
-			if( $type != 'sqlite' && $type != 'sqlite3' ) {
-				self::$preparedQueries[$rawQuery] = $result;
-			}
 		}
 		return $result;
 	}
@@ -280,7 +270,6 @@ class OC_DB {
 		// Cut connection if required
 		if(self::$connection) {
 			self::$connection->close();
-			self::$preparedQueries = array();
 		}
 
 		return true;
@@ -612,10 +601,11 @@ class OC_DB {
 	 * @param bool $enabled
 	 */
 	static public function enableCaching($enabled) {
-		if (!$enabled) {
-			self::$preparedQueries = array();
+		if ($enabled) {
+			self::$connection->enableQueryStatementCaching();
+		} else {
+			self::$connection->disableQueryStatementCaching();
 		}
-		self::$cachingEnabled = $enabled;
 	}
 }
 
